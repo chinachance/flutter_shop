22802d51bcf6dafa8341bcd9bde379f8938dd388
hao <1451295544@qq.com>
2019-06-04 17:02:57 +0800

    首页listview假数据

--- lib/learn.dart
+++ lib/learn.dart
@@ -7,14 +7,38 @@ class LearnPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body:,
+      body: ListView(
+        padding: const EdgeInsets.all(10.0),
+        children: <Widget>[
+          _CustomListItemTwo(
+            thumbnail: Container(
+              decoration: const BoxDecoration(color: Colors.pink),
+            ),
+            title: 'shop',
+            subtitle:
+                'Flutter continues to improve and expand its horizons.This text should max out at two lines and clip',
+            author: 'hao',
+            publishDate: '2019-6-4',
+            readDuration: '5 mins',
+          ),
+          _CustomListItemTwo(
+            thumbnail: Container(
+              decoration: const BoxDecoration(color: Colors.blue),
+            ),
+            title: 'shop',
+            subtitle: 'Flutter once again improves and makes updates.',
+            author: 'hao',
+            publishDate: '2019-6-4',
+            readDuration: '5 mins',
+          )
+        ],
+      ),
     );
   }
 }
 
-class _LearnList extends StatelessWidget {
-
-  _LearnList({
+class _ArticleDescription extends StatelessWidget {
+  _ArticleDescription({
     Key key,
     this.title,
     this.subtitle,
@@ -29,11 +53,103 @@ class _LearnList extends StatelessWidget {
   final String publishDate;
   final String readDuration;
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                '$title',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 2)),
+              Text(
+                '$subtitle',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(fontSize: 12.0, color: Colors.black54),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                '$author',
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black87,
+                ),
+              ),
+              Text(
+                '$publishDate · $readDuration ★',
+                style: const TextStyle(fontSize: 12.0, color: Colors.black54),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _CustomListItemTwo extends StatelessWidget {
+  _CustomListItemTwo(
+      {Key key,
+      this.thumbnail,
+      this.title,
+      this.subtitle,
+      this.author,
+      this.publishDate,
+      this.readDuration})
+      : super(key: key);
+
+  final Widget thumbnail;
+  final String title;
+  final String subtitle;
+  final String author;
+  final String publishDate;
+  final String readDuration;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 1.0,
+              child: thumbnail,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
+                child: _ArticleDescription(
+                  title: title,
+                  subtitle: subtitle,
+                  author: author,
+                  publishDate: publishDate,
+                  readDuration: readDuration,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
-
 }
